429e29bb426d25a1f0f2c5df50595667fdd40315
xiazhongwei <xiazhongwei@able-elec.com>
2017-12-05 19:20:48 +0800

    spring4.x--->ContextRefreshedEvent

---
+++ event/src/main/java/com/sishuok/hello/SpringContextRefrreshedEventListener.java
@@ -0,0 +1,18 @@
+package com.sishuok.hello;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2017/12/5.
+ */
+@Component
+public class SpringContextRefrreshedEventListener implements ApplicationListener<ContextRefreshedEvent> {
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        if (contextRefreshedEvent.getApplicationContext().getParent() == null) {
+            System.out.println("SpringContextRefrreshedEventListener---->");
+        }
+    }
+}
